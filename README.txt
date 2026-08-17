3cfaf856ba31d2f1b4c3fce20362b1b3481ab189
Kyran Davis-Eyre <Kyran.daviseyre@hotmail.co.uk>
2026-08-17 21:51:17 +0100

    added a get average function for running or cycling average speed or pace

---
+++ README.txt
@@ -0,0 +1,11 @@
+# This is pace, a personal project to put my engineering knowledge into a real work practical application.
+
+## While this application is going to be used to help me build my knowlege of multiple back end engineering concepts it will also be 
+    as an experiment to see how far i can delveop my skills
+
+### Current features are light, i can take data defined in the app.js file and add them to an array...
+
+#### In the near future the plan is to store the activivtes defined in the app in a json file, eventually moving to an SQL lite data base
+    I will also be looking to integrate RESTApi and eventually a front end to this application
+
+##### The current technoligies are node.js and javascript.
--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 console.log(`Application starting - "P A C E"`);
+//---------------------------------------------
+// USER PREFERENCES
+//---------------------------------------------
 
-//User and preferences
 const user = {
     name: "Kyran_DE",
     age: 29,
@@ -13,23 +15,94 @@ const user = {
     }
 }
 
+//---------------------------------------------
+// FUNCTIONS
+//---------------------------------------------
+
 //Add activity
 function addActivity(type, distance, distanceUnit, time) {
     return{
-        type : type,
+        type : type.toUpperCase(),
         distance : distance, 
-        distanceUnit : distanceUnit,
-        timeInSecconds : time,
+        distanceUnit : distanceUnit.toUpperCase(),
+        timeInSeconds : time,
         dateStamp : new  Date()
     };
 }
 
-const newActivity = addActivity("Cycle", 25, "KM", 3000);
+//Distance conversion - kilometers to miles
+function kilometersToMiles(kilometers){
+    return kilometers / 1.6;
+}
+
+//Distance conversion - miles to kilometers
+function milesToKilometers (miles){
+    return miles * 1.6;
+}
+
+//Get activities - filters: type, distance
+function getActivities(activityType, distanceFilter = 0){
+    const activityClean = activityType.toUpperCase();
+    const filteredActivities = [];
+    for(let i=0; i<activities.length; i++){
+        if(activities[i].type === activityClean && activities[i].distance >= distanceFilter){
+            filteredActivities.push(activities[i]);
+        }
+    }
+    return filteredActivities;
+}
+
+//Caclulate average run pace - takes an array of running activities
+function calculateAvgRun(runActivities){
+    let totalDistance = 0;
+    let totalTime = 0;
+
+    for(let i=0; i<runActivities.length; i++){
+        totalDistance += runActivities[i].distance;
+        totalTime += runActivities[i].timeInSeconds;
+    }
+    const averageRunPace = totalTime / totalDistance; 
+    return averageRunPace
+}
+
+//Caclulate average cycle speed - takes an array of cycling activities
+function calculateAvgCycle(cycleActivities){
+    let totalDistance = 0;
+    let totalTime = 0;
+
+    for(let i= 0; i<cycleActivities.length; i++){
+        totalDistance += cycleActivities[i].distance;
+        totalTime += cycleActivities[i].timeInSeconds;
+    }
+    const cyclingAverage = totalDistance / (totalTime / 3600);
+    return cyclingAverage;
+}
+
+//Get the average pace / speed for activities - takes the activity type and minimum distance filter
+function getAverage(activityType, filterDistance = 0){
+    if(activityType.toUpperCase() === "RUN"){
+        const averagePace = calculateAvgRun(getActivities(activityType, filterDistance));
+        return averagePace;
+    }
+    if(activityType.toUpperCase() === "CYCLE"){
+        const averageSpeed = calculateAvgCycle(getActivities(activityType, filterDistance));
+        return averageSpeed;
+    }
+}
+
+//Manually enter my acticity - temp will be UI down the line
+const newActivity = addActivity("cycle", 25, "KM", 3000);
+const newActivityTwo = addActivity("run", 10, "KM", 3600);
+const newActivityThree = addActivity("Cycle", 50, "KM", 7200);
+const newActivityFour = addActivity("Run", 5, "KM", 1500);
 
 //Array of activities - temp will be pushed to DB down the line
 const activities = [];
 
-//Add activity to the activities arrat
+//Add activity to the activities array - temp this will be DB stroage down the line
 activities.push(newActivity);
+activities.push(newActivityTwo);
+activities.push(newActivityThree);
+activities.push(newActivityFour);
 
 console.log(`Application ending - "P A C E"`);
